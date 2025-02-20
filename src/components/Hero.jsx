db95f964ae2d85854d2444b1c6ef05c070874ffa
Nishantmalik113 <nishantmalik113@gmail.com>
2025-02-20 18:39:53 +0530

    overflow hidden

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ export default function Hero(props) {
 
   
   return (
-    <section  className='px-10 flex flex-col gap-5 sm:gap-30 items-center justify-center bg-[radial-gradient(circle at 50% , #3F3F47, #18181B)]'>
+    <section  className='px-10 overflow-hidden flex flex-col gap-5 sm:gap-30 items-center justify-center bg-[radial-gradient(circle at 50% , #3F3F47, #18181B)]'>
         <WelcomePage/>
         <div className='absolute top-[110vh] md:top-[90vh] left-[10vh] md:left-[20vh] z-20 bg-gradient-to-bl'><img alt="square" loading="lazy" width="96" height="96" decoding="async" data-nimg="1" src="../square.svg" /></div>
         <div className='absolute top-[130vh] md:top-[110vh] left-0 z-20 bg-gradient-to-bl'><img alt="square" loading="lazy" width="96" height="96" decoding="async" data-nimg="1" src="../square.svg" /></div>
